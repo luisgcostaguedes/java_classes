ae67de5f4eb5937bdff91084e869a21e23f51ea5
luisgcostaguedes <luisgcostaguedes@gmail.com>
2024-03-20 17:11:30 -0300

    up

---
+++ class/src/Concatenacao.java
@@ -0,0 +1,9 @@
+public class Concatenacao {
+    public static void main(String[] args) throws Exception {
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Alice ";
+        String continuacao = "e minha idade é ";
+        int idade = 17;
+        System.out.println(saudacao + nome + continuacao + idade);
+    }
+}
